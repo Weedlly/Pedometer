09c0ef475d1f444bfafb07773dbcb207e2f381eb
Weed <phamhuyhoang3004@gmail.com>
2022-08-31 14:14:21 +0700

    Calculating time, distance, calories between two point

--- app/src/main/java/com/example/pedometer/GpsMap.kt
+++ app/src/main/java/com/example/pedometer/GpsMap.kt
@@ -2,14 +2,11 @@ package com.example.pedometer
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -24,14 +21,14 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.Place.Field
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
 import com.google.android.libraries.places.api.net.PlacesClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
+private const val baseUrl = "https://api.mapbox.com"
 
 
 class GpsMap : AppCompatActivity(), OnMapReadyCallback {
@@ -52,17 +49,17 @@ class GpsMap : AppCompatActivity(), OnMapReadyCallback {
 
     // A default location (Sydney, Australia) and default zoom to use when location permission is
     // not granted.
-    private val defaultLocation = LatLng(22.0, 102.0)
     private var locationPermissionGranted = false
 
     // The geographical location where the device is currently located. That is, the last-known
     // location retrieved by the Fused Location Provider.
 
     private var lastKnownLocation: Location? = null
-    private var likelyPlaceNames: Array<String?> = arrayOfNulls(0)
-    private var likelyPlaceAddresses: Array<String?> = arrayOfNulls(0)
-    private var likelyPlaceAttributions: Array<List<*>?> = arrayOfNulls(0)
-    private var likelyPlaceLatLngs: Array<LatLng?> = arrayOfNulls(0)
+
+//    private var likelyPlaceNames: Array<String?> = arrayOfNulls(0)
+//    private var likelyPlaceAddresses: Array<String?> = arrayOfNulls(0)
+//    private var likelyPlaceAttributions: Array<List<*>?> = arrayOfNulls(0)
+//    private var likelyPlaceLatLngs: Array<LatLng?> = arrayOfNulls(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -97,6 +94,7 @@ class GpsMap : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         this.mGoogleMap = googleMap
+//        mGoogleMap!!.uiSettings.isZoomControlsEnabled = true
 
         // [START_EXCLUDE]
         // [START map_current_place_set_info_window_adapter]
@@ -130,11 +128,26 @@ class GpsMap : AppCompatActivity(), OnMapReadyCallback {
 
         // Get the current location of the device and set the position of the map.
         getDeviceLocation()
-        // Add a marker in Sydney and move the camera
-//        val kyoto = LatLng(35.00116, 135.7681)
-//        mGoogleMap.addMarker(MarkerOptions().position(kyoto).title("Marker in Tokyo"))
-//        mGoogleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
-//        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(kyoto))
+
+        mGoogleMap?.setOnMyLocationButtonClickListener {
+            val latLng = LatLng(lastKnownLocation!!.latitude,lastKnownLocation!!.longitude)
+            Log.v("GPS","Current location: $lastKnownLocation")
+            mGoogleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f))
+            val markerOptions = MarkerOptions()
+            if (markerPoints.isEmpty()) {
+                markerPoints.add(latLng)
+            }
+            else {
+                markerPoints.clear()
+                mGoogleMap!!.clear()
+                markerPoints.add(latLng)
+            }
+            markerOptions.position(latLng)
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+            mGoogleMap!!.addMarker(markerOptions.title("You are here"))
+            true
+        }
+
         mGoogleMap!!.setOnMapClickListener {
             if (markerPoints.size > 1){
                 markerPoints.clear()
@@ -169,20 +182,20 @@ class GpsMap : AppCompatActivity(), OnMapReadyCallback {
                 )
 
                 val coordinates = getDirection(origin,dest)
-                println(coordinates)
+
                 var nextStep = origin
                 for (step in coordinates){
                     val lng = LatLng(step[1],step[0])
                     polylineOptions.add(nextStep,lng)
                     nextStep = lng
                 }
+
                 polylineOptions.add(nextStep,dest)
                 mGoogleMap!!.addPolyline(polylineOptions)
+
             }
         }
     }
-    private val baseUrl = "https://api.mapbox.com"
-
 
     private fun getDirection(origin: LatLng ,dest: LatLng) : List<List<Double>>{
         val retrofit = Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build()
@@ -227,7 +240,6 @@ class GpsMap : AppCompatActivity(), OnMapReadyCallback {
 
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        println("move to current place")
         menuInflater.inflate(R.menu.current_place_menu, menu)
         return true
     }
@@ -273,37 +285,24 @@ class GpsMap : AppCompatActivity(), OnMapReadyCallback {
     @SuppressLint("MissingPermission")
     private fun getDeviceLocation() {
         try {
-            println("get_divice_location")
             if(locationPermissionGranted){
                 val locationResult = fusedLocationProviderClient.lastLocation
                 locationResult.addOnCompleteListener(this){
                     task ->
-
-                    //var currentLatLng = LatLng(lastKnownLocation!!.longitude,lastKnownLocation!!.latitude)
-                    if(task.isSuccessful){
-                        println("set_camera")
+                    if(task.isSuccessful) {
                         // Set the map's camera position to the current location of the device.
                         lastKnownLocation = task.result
-
-//                        println("lastKnownLocation : $lastKnownLocation")
-//                        val markerOptions = MarkerOptions()
-//                        markerOptions.position(LatLng( lastKnownLocation!!.longitude,lastKnownLocation!!.latitude))
-//                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-//                        mGoogleMap!!.addMarker(markerOptions)
-                        if (lastKnownLocation != null){
-                            mGoogleMap?.moveCamera((CameraUpdateFactory.newLatLngZoom(
-                                LatLng(lastKnownLocation!!.latitude,
-                                    lastKnownLocation!!.altitude), DEFAULT_ZOOM.toFloat())))
+                        if (lastKnownLocation != null) {
+                            mGoogleMap?.moveCamera(
+                                (CameraUpdateFactory.newLatLngZoom(
+                                    LatLng(
+                                        lastKnownLocation!!.latitude,
+                                        lastKnownLocation!!.altitude
+                                    ), DEFAULT_ZOOM.toFloat()
+                                ))
+                            )
                         }
                     }
-//                    markerPoints.add( LatLng(lastKnownLocation!!.latitude,lastKnownLocation!!.))
-//                    // Creating MarkerOptions
-//                    val markerOptions = MarkerOptions()
-//                    // Setting the position of the marker
-//                    markerOptions.position(it)
-//                    if (markerPoints.size == 1)
-//                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-
                 }
             }
         }
@@ -312,115 +311,115 @@ class GpsMap : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.option_get_place){
-            println("show_current")
-            showCurrentPlace()
-        }
-        return true
-    }
-    @SuppressLint("MissingPermission")
-    private fun showCurrentPlace(){
-        println("show_current")
-        if(mGoogleMap == null){
-            return
-        }
-        println(locationPermissionGranted)
-        if (locationPermissionGranted){
-            // Use fields to define the data types to return.
-            val placeFields = listOf(Field.NAME, Field.ADDRESS, Field.LAT_LNG)
-            // Use the builder to create a FindCurrentPlaceRequest.
-            val request = FindCurrentPlaceRequest.newInstance(placeFields)
-            // Get the likely places - that is, the businesses and other points of interest that
-            // are the best match for the device's current location.
-            val placeResult = placesClient.findCurrentPlace(request)
-            placeResult.addOnCompleteListener{task->
-                if (task.isSuccessful && task.result != null){
-                    val likelyPlaces = task.result
-                    val count = if (likelyPlaces != null && likelyPlaces.placeLikelihoods.size < M_MAX_ENTRIES) {
-                        likelyPlaces.placeLikelihoods.size
-                    }else{
-                        M_MAX_ENTRIES
-                    }
-                    var i = 0
-                    likelyPlaceNames = arrayOfNulls(count)
-                    likelyPlaceAddresses = arrayOfNulls(count)
-                    likelyPlaceAttributions = arrayOfNulls<List<*>?>(count)
-                    likelyPlaceLatLngs = arrayOfNulls(count)
-                    for (placeLikelihood in likelyPlaces?.placeLikelihoods ?: emptyList()) {
-                        // Build a list of likely places to show the user.
-                        likelyPlaceNames[i] = placeLikelihood.place.name?.toString()
-                        likelyPlaceAddresses[i] = placeLikelihood.place.address
-                        likelyPlaceAttributions[i] = placeLikelihood.place.attributions
-                        likelyPlaceLatLngs[i] = placeLikelihood.place.latLng
-                        println("stt: $i ")
-                        print(" ${likelyPlaceLatLngs[i]!!.latitude}")
-                        println(" and  ${likelyPlaceLatLngs[i]!!.longitude}")
-                        i++
-                        if (i > count - 1) {
-                            break
-                        }
-                    }
-
-                    // Show a dialog offering the user the list of likely places, and add a
-                    // marker at the selected place.
-                    openPlacesDialog()
-                } else {
-                    Log.e(TAG, "Exception: %s", task.exception)
-                }
-
-            }
-        }
-        else {
-            // The user has not granted permission.
-            Log.i(TAG, "The user did not grant location permission.")
-
-            // Add a default marker, because the user hasn't selected a place.
-            mGoogleMap?.addMarker(MarkerOptions()
-                .title(getString(R.string.default_info_title))
-                .position(defaultLocation)
-                .snippet(getString(R.string.default_info_snippet)))
-
-            // Prompt the user for permission.
-            getLocationPermission()
-        }
-    }
-    private fun openPlacesDialog() {
-
-        // Ask the user to choose the place where they are now.
-        val listener = DialogInterface.OnClickListener { _, which -> // The "which" argument contains the position of the selected item.
-            val markerLatLng = likelyPlaceLatLngs[which]
-            var markerSnippet = likelyPlaceAddresses[which]
-            if (likelyPlaceAttributions[which] != null) {
-                markerSnippet = """
-                    $markerSnippet
-                    ${likelyPlaceAttributions[which]}
-                    """.trimIndent()
-            }
-
-            if (markerLatLng == null) {
-                return@OnClickListener
-            }
-
-            // Add a marker for the selected place, with an info window
-            // showing information about that place.
-
-            mGoogleMap?.addMarker(MarkerOptions()
-                .title(likelyPlaceNames[which])
-                .position(markerLatLng)
-                .snippet(markerSnippet))
-
-            // Position the map's camera at the location of the marker.
-            mGoogleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(markerLatLng,
-                DEFAULT_ZOOM.toFloat()))
-        }
-
-        // Display the dialog.
-        AlertDialog.Builder(this)
-            .setTitle(R.string.pick_place)
-            .setItems(likelyPlaceNames, listener)
-            .show()
-    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        if (item.itemId == R.id.option_get_place){
+//            println("show_current")
+//            showCurrentPlace()
+//        }
+//        return true
+//    }
+//    @SuppressLint("MissingPermission")
+//    private fun showCurrentPlace(){
+//        println("show_current")
+//        if(mGoogleMap == null){
+//            return
+//        }
+//        println(locationPermissionGranted)
+//        if (locationPermissionGranted){
+//            // Use fields to define the data types to return.
+//            val placeFields = listOf(Field.NAME, Field.ADDRESS, Field.LAT_LNG)
+//            // Use the builder to create a FindCurrentPlaceRequest.
+//            val request = FindCurrentPlaceRequest.newInstance(placeFields)
+//            // Get the likely places - that is, the businesses and other points of interest that
+//            // are the best match for the device's current location.
+//            val placeResult = placesClient.findCurrentPlace(request)
+//            placeResult.addOnCompleteListener{task->
+//                if (task.isSuccessful && task.result != null){
+//                    val likelyPlaces = task.result
+//                    val count = if (likelyPlaces != null && likelyPlaces.placeLikelihoods.size < M_MAX_ENTRIES) {
+//                        likelyPlaces.placeLikelihoods.size
+//                    }else{
+//                        M_MAX_ENTRIES
+//                    }
+//                    var i = 0
+//                    likelyPlaceNames = arrayOfNulls(count)
+//                    likelyPlaceAddresses = arrayOfNulls(count)
+//                    likelyPlaceAttributions = arrayOfNulls<List<*>?>(count)
+//                    likelyPlaceLatLngs = arrayOfNulls(count)
+//                    for (placeLikelihood in likelyPlaces?.placeLikelihoods ?: emptyList()) {
+//                        // Build a list of likely places to show the user.
+//                        likelyPlaceNames[i] = placeLikelihood.place.name?.toString()
+//                        likelyPlaceAddresses[i] = placeLikelihood.place.address
+//                        likelyPlaceAttributions[i] = placeLikelihood.place.attributions
+//                        likelyPlaceLatLngs[i] = placeLikelihood.place.latLng
+//                        println("stt: $i ")
+//                        print(" ${likelyPlaceLatLngs[i]!!.latitude}")
+//                        println(" and  ${likelyPlaceLatLngs[i]!!.longitude}")
+//                        i++
+//                        if (i > count - 1) {
+//                            break
+//                        }
+//                    }
+//
+//                    // Show a dialog offering the user the list of likely places, and add a
+//                    // marker at the selected place.
+//                    openPlacesDialog()
+//                } else {
+//                    Log.e(TAG, "Exception: %s", task.exception)
+//                }
+//
+//            }
+//        }
+//        else {
+//            // The user has not granted permission.
+//            Log.i(TAG, "The user did not grant location permission.")
+//
+//            // Add a default marker, because the user hasn't selected a place.
+//            mGoogleMap?.addMarker(MarkerOptions()
+//                .title(getString(R.string.default_info_title))
+//                .position(defaultLocation)
+//                .snippet(getString(R.string.default_info_snippet)))
+//
+//            // Prompt the user for permission.
+//            getLocationPermission()
+//        }
+//    }
+//    private fun openPlacesDialog() {
+//
+//        // Ask the user to choose the place where they are now.
+//        val listener = DialogInterface.OnClickListener { _, which -> // The "which" argument contains the position of the selected item.
+//            val markerLatLng = likelyPlaceLatLngs[which]
+//            var markerSnippet = likelyPlaceAddresses[which]
+//            if (likelyPlaceAttributions[which] != null) {
+//                markerSnippet = """
+//                    $markerSnippet
+//                    ${likelyPlaceAttributions[which]}
+//                    """.trimIndent()
+//            }
+//
+//            if (markerLatLng == null) {
+//                return@OnClickListener
+//            }
+//
+//            // Add a marker for the selected place, with an info window
+//            // showing information about that place.
+//
+//            mGoogleMap?.addMarker(MarkerOptions()
+//                .title(likelyPlaceNames[which])
+//                .position(markerLatLng)
+//                .snippet(markerSnippet))
+//
+//            // Position the map's camera at the location of the marker.
+//            mGoogleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(markerLatLng,
+//                DEFAULT_ZOOM.toFloat()))
+//        }
+//
+//        // Display the dialog.
+//        AlertDialog.Builder(this)
+//            .setTitle(R.string.pick_place)
+//            .setItems(likelyPlaceNames, listener)
+//            .show()
+//    }
     companion object {
         private val TAG = GpsMap::class.java.simpleName
         private const val DEFAULT_ZOOM = 0
